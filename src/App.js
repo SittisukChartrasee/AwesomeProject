58288762a1bcd008d8e16eeaf1ecf47a9f85f20f
sittisuk <sittisuk@codefin.io>
2019-12-27 17:20:36 +0700

    add react navigation

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import AppNavigator from './config/router'
+import { createAppContainer } from 'react-navigation'
+
+const AppContainer = createAppContainer(AppNavigator);
+
+export default () => {
+  return <AppContainer />
+}
